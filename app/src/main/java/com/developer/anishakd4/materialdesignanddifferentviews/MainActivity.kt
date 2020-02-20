bbc65633749b0057d1ed5ba73a8729cc864804ef
Anish <anishakd4@gmail.com>
2020-02-20 07:48:07 +0530

    init

---
+++ app/src/main/java/com/developer/anishakd4/materialdesignanddifferentviews/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.developer.anishakd4.materialdesignanddifferentviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
